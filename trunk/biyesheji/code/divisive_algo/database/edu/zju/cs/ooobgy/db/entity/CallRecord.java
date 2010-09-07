3c067ec67f668cadbf5c9c9af1a12f8375c855f2
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-09-07 10:28:53 +0000

    callRecord

--- trunk/biyesheji/code/divisive_algo/database/edu/zju/cs/ooobgy/db/entity/CallRecord.java
+++ trunk/biyesheji/code/divisive_algo/database/edu/zju/cs/ooobgy/db/entity/CallRecord.java
@@ -6,6 +6,60 @@
 
 package edu.zju.cs.ooobgy.db.entity;
 
+import java.sql.Timestamp;
+
+//create table HD_DEMO
+//(
+//  CALLER_NUM    VARCHAR2(50),
+//  RECEIVER_NUM  VARCHAR2(50),
+//  START_TIME    DATE,
+//  SWITCH_ID     VARCHAR2(50),
+//  STATION_CODE  VARCHAR2(50),
+//  CALL_DURATION NUMBER
+//);
 public class CallRecord {
+	private String callerNum;
+	private String receiverNum;
+	private Timestamp startTime;
+	private String stationId;
+	private String stationCode;
+	private Double callDuration;
+	public String getCallerNum() {
+		return callerNum;
+	}
+	public void setCallerNum(String callerNum) {
+		this.callerNum = callerNum;
+	}
+	public String getReceiverNum() {
+		return receiverNum;
+	}
+	public void setReceiverNum(String receiverNum) {
+		this.receiverNum = receiverNum;
+	}
+	public Timestamp getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Timestamp startTime) {
+		this.startTime = startTime;
+	}
+	public String getStationId() {
+		return stationId;
+	}
+	public void setStationId(String stationId) {
+		this.stationId = stationId;
+	}
+	public String getStationCode() {
+		return stationCode;
+	}
+	public void setStationCode(String stationCode) {
+		this.stationCode = stationCode;
+	}
+	public Double getCallDuration() {
+		return callDuration;
+	}
+	public void setCallDuration(Double callDuration) {
+		this.callDuration = callDuration;
+	}
+	
 	
 }
