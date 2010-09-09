519abe163900d12f5abf07ca105c1fbe1c700e60
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-09-09 03:41:50 +0000

    entity

--- trunk/biyesheji/code/divisive_algo/database/edu/zju/cs/ooobgy/db/entity/RelationWeighted.java
+++ trunk/biyesheji/code/divisive_algo/database/edu/zju/cs/ooobgy/db/entity/RelationWeighted.java
@@ -7,5 +7,34 @@
 package edu.zju.cs.ooobgy.db.entity;
 
 public class RelationWeighted {
-
+	private Integer rid;
+	private Integer callerId;
+	private Integer receiverId;
+	private Integer weight;
+	public Integer getRid() {
+		return rid;
+	}
+	public void setRid(Integer rid) {
+		this.rid = rid;
+	}
+	public Integer getCallerId() {
+		return callerId;
+	}
+	public void setCallerId(Integer callerId) {
+		this.callerId = callerId;
+	}
+	public Integer getReceiverId() {
+		return receiverId;
+	}
+	public void setReceiverId(Integer receiverId) {
+		this.receiverId = receiverId;
+	}
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	
+	
 }
