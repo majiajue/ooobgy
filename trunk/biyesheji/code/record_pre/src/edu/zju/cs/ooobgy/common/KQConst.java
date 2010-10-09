2cf69ca74734bc2aa9b045040c13a0922a50abad
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-09 04:55:34 +0000

    分离日期和小时

--- trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/common/KQConst.java
+++ trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/common/KQConst.java
@@ -3,4 +3,5 @@ package edu.zju.cs.ooobgy.common;
 public class KQConst {
 	public static final String ORIGINAL_SPLIT = "\t";
 	public static final String COMMON_SPLIT = "\u0001";
+	public static final String SPACE_SPLIT = " ";
 }
