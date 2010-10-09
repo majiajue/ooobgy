1e13e9dd318390cfd3219c0d7ceb50b780ea3cfe
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-09 06:50:29 +0000

    脚本调整

--- biyesheji/code/record_pre/shell/date_filter.sh
+++ biyesheji/code/record_pre/shell/date_filter.sh
@@ -7,12 +7,18 @@ then
     export jobconf_file=$1
 fi
 
+date_filter=0
+if [ $# -eq 2 ];
+then
+    export date_filter=$2
+fi
+
 class_root=edu.zju.cs.ooobgy.mr
 laucher=$class_root.common.HadoopLauncher
 job_class=$class_root.jobs.DateFilter
 jar_path=../dist/record_base_mr.jar
 hadoop_cmd=hadoop
 
-out_path=/group/taobao/ooobgy/call_record/record_base
+out_path=/group/taobao/ooobgy/call_record/date_filter/$date_filter
 $hadoop_cmd fs -rmr $out_path
-$hadoop_cmd jar $jar_path $laucher $job_class $jobconf_file
+$hadoop_cmd jar $jar_path $laucher $job_class $jobconf_file date_filter=$date_filter
