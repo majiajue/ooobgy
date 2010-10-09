956f37dfc370459ea3114ede306b5be4435ae811
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-09 09:02:31 +0000

    weight shell

---
+++ trunk/biyesheji/code/record_pre/shell/weight_record.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export jobconf_file=RecordBase.xml
+
+if [ $# -eq 1 ];
+then
+    export jobconf_file=$1
+fi
+
+class_root=edu.zju.cs.ooobgy.mr
+laucher=$class_root.common.HadoopLauncher
+job_class=$class_root.jobs.WeightRecord
+jar_path=../dist/record_base_mr.jar
+hadoop_cmd=hadoop
+
+out_path=/group/taobao/ooobgy/call_record/weight
+$hadoop_cmd fs -rmr $out_path
+$hadoop_cmd jar $jar_path $laucher $job_class $jobconf_file
