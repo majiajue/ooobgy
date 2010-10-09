af40663e43d741ee286cf2fa0d48f5d145ee0fc4
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-09 03:17:17 +0000

    基本数据MapReduce

---
+++ trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/common/HadoopLauncher.java
@@ -0,0 +1,130 @@
+package edu.zju.cs.ooobgy.mr.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * @Author: 周晓龙
+ * @created: 2010-7-21
+ **/
+
+public class HadoopLauncher {
+	private Map<String, String> configMap = new HashMap<String, String>();
+
+	public static void main(String[] args) {
+		HadoopLauncher launcher = new HadoopLauncher();
+		String configFilePath = null;
+		if (args.length < 1) {
+			System.err
+					.println("Usage : HadoopLauncher [HadoopJobClass] {[Date(yyyyMMdd)]} {[ConfigFile]}");
+			System.exit(1);
+		} else if (args.length == 1 || args.length == 2) {
+			String[] detail = args[0].split("\\.");
+			String configFile = detail[detail.length - 1];
+			configFilePath = configFile + ".xml";// 默认载入与JobClass同名同路径的配置文件
+		} else if (args.length == 3) {
+			configFilePath = args[2];
+		}
+		HadoopJob hadoopJob = null;
+		try {
+			hadoopJob = (HadoopJob) Class.forName(args[0]).newInstance();
+		} catch (Exception e) {
+			exitWithError(e, "Invalid HadoopJob Class");
+		}
+		JobConf conf = new JobConf(hadoopJob.getClass());
+		conf.setMapperClass(hadoopJob.getMapper());
+		conf.setReducerClass(hadoopJob.getReducer());
+		launcher.configFromFile(configFilePath, conf);// 先从文件配置
+		if (args.length == 2 || args.length == 3) {
+			conf.set("job_date", args[1]);
+		}
+		hadoopJob.configJob(conf);// 再从程序配置
+		try {
+			JobClient.runJob(conf);
+		} catch (IOException e) {
+			exitWithError(e, "Error : Loading Job error!");
+		}
+	}
+
+	/**
+	 * 读入配置文件对HadoopJob进行配置
+	 * 
+	 * @see HadoopLauncher#configFromFile(String)
+	 * @param configFilePath
+	 * @param jobConf
+	 * @return jobConf
+	 */
+	public JobConf configFromFile(String configFilePath, JobConf jobConf) {
+		loadConfigFile(configFilePath);
+		Set<String> configs = configMap.keySet();
+		for (String config : configs) {
+			jobConf.set(config, configMap.get(config));
+		}
+		return jobConf;
+	}
+
+	/**
+	 * 使用该方法从文件配置Job会new一个JobConf，配置后返回该JobConf
+	 * 
+	 * @see HadoopLauncher#configFromFile(String, JobConf)
+	 * @param configFilePath
+	 * @return jobConf
+	 */
+	public JobConf configFromFile(String configFilePath) {
+		return configFromFile(configFilePath, new JobConf());
+	}
+
+	/**
+	 * 从配置文件(xml文件)加载配置信息
+	 * 
+	 * @param configFilePath
+	 */
+	private void loadConfigFile(String configFilePath) {
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
+			Document doc = documentBuilder.parse(new File(configFilePath));
+			Element root = doc.getDocumentElement();
+			NodeList properties = root.getElementsByTagName("property");
+			Element property;
+			String name;
+			String value;
+			for (int i = 0; i < properties.getLength(); i++) {
+				property = (Element) properties.item(i);
+				name = property.getElementsByTagName("name").item(0)
+						.getFirstChild().getNodeValue();
+				value = property.getElementsByTagName("value").item(0)
+						.getFirstChild().getNodeValue();
+				this.configMap.put(name, value);
+			}
+		} catch (ParserConfigurationException e) {
+			exitWithError(e, "Error : Can't load config parser!");
+		} catch (SAXException e) {
+			exitWithError(e,
+					"Error : Parse config error!(Maybe xml file is not in the correct format)");
+		} catch (IOException e) {
+			exitWithError(e, "Error : Invalid config file.(file not exist)");
+		}
+	}
+
+	private static void exitWithError(Exception e, String errorInfo) {
+		System.err.println(errorInfo);
+		e.printStackTrace();
+		System.exit(1);
+	}
+}
---
+++ trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/jobs/RecordBase.java
@@ -0,0 +1,99 @@
+package edu.zju.cs.ooobgy.mr.jobs;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+import edu.zju.cs.ooobgy.common.KQConst;
+import edu.zju.cs.ooobgy.mr.common.HadoopJob;
+
+/**
+ * 处理原始通话记录数据，生成统一格式的基础记录数据
+ * 
+ * @author 周晓龙
+ * @created 2010-10-9
+ */
+public class RecordBase implements HadoopJob {
+
+	public static class TopMapper extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, Text> {
+
+		@Override
+		public void configure(JobConf job) {
+			super.configure(job);
+		}
+
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+			String line = value.toString();
+			String[] items = line.split(KQConst.ORIGINAL_SPLIT);
+			String caller = items[0];// 主叫号码代号
+			String reciever = items[2];// 被叫号码代号
+			String timeStamp = items[1];// 通话开始时间
+			String switcher = items[3];// 交换机编号
+			String baseStation = items[4];// 基站编号
+			String callTime = items[5];// 通话时长(秒)
+
+			StringBuilder keyBuilder = new StringBuilder();
+			keyBuilder.append(timeStamp).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(caller).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(reciever).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(callTime).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(switcher).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(baseStation);
+
+			output.collect(new Text(keyBuilder.toString()), new Text());
+		}
+
+	}
+
+	/**
+	 * Unused
+	 * this job need no reducer
+	 * @author 周晓龙
+	 * @created 2010-10-9
+	 */
+	public static class TopReducer extends MapReduceBase implements
+			Reducer<Text, Text, Text, Text> {
+		@Override
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+			while(values.hasNext()){
+				output.collect(key, values.next());
+			}
+		}
+	}
+
+	@Override
+	public void configJob(JobConf conf) {
+		conf.setMapOutputKeyClass(Text.class);
+		conf.setMapOutputValueClass(Text.class);
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Class<? extends Mapper> getMapper() {
+		return TopMapper.class;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Class<? extends Reducer> getReducer() {
+		return TopReducer.class;
+	}
+
+}
