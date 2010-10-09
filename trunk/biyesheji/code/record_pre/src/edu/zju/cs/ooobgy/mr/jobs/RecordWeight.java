69e4b52c0640712a9441875c85432a1a9d27f300
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-09 08:49:19 +0000

    weight

--- trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/jobs/RecordWeight.java
+++ trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/jobs/RecordWeight.java
@@ -1,29 +1,139 @@
 package edu.zju.cs.ooobgy.mr.jobs;
 
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.Reducer;
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+import edu.zju.cs.ooobgy.common.KQConst;
 import edu.zju.cs.ooobgy.mr.common.HadoopJob;
 
+/**
+ * 归一化两个人之间的通话次数，通话时间，
+ * 归一权值 output_format: 主叫^A被叫^A通话次数^A通话总时间（秒）^A归一化权值
+ * 
+ * @author 周晓龙
+ * @created 2010-10-9
+ */
 public class RecordWeight implements HadoopJob {
+
+	/**
+	 * 权值计算公式 weight = 60 + (第1分钟内的秒数)/1 + (第2分钟内的秒数)/2 + ...
+	 * 
+	 * @return weight
+	 */
+	public static int weightEvaluate(int time) {
+		int weight = 60;
+		int min = 1;
+		while (time > 0) {
+			int tail = time;
+			time -= 60;
+			if (time > 0) {
+				tail = 60;
+			}
+			weight = weight + tail / min;
+			min++;
+		}
+
+		return weight;
+	}
+
+	public static class TopMapper extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, Text> {
+
+		@Override
+		public void configure(JobConf job) {
+			super.configure(job);
+		}
+
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+			String line = value.toString();
+			String[] items = line.split(KQConst.COMMON_SPLIT);
+			String caller = items[2];
+			String reciever = items[3];
+			String time = items[4];
+
+			StringBuilder keyBuilder = new StringBuilder();
+			keyBuilder.append(caller).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(reciever);
+			StringBuilder valueBuilder = new StringBuilder();
+			valueBuilder.append(time);
+
+			output.collect(new Text(keyBuilder.toString()), new Text(
+					valueBuilder.toString()));
+		}
+
+	}
+
+	/**
+	 * 统计通话次数，通话时间和权值
+	 * 
+	 * @author 周晓龙
+	 * @created 2010-10-9
+	 */
+	public static class TopReducer extends MapReduceBase implements
+			Reducer<Text, Text, Text, Text> {
+		
+		@Override
+		public void configure(JobConf job) {
+			super.configure(job);
+		}
+		
+		@Override
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+			int callCount = 0;
+			int totalTime = 0;
+			int weight = 0;
+			while (values.hasNext()) {
+				try {
+					int time = Integer.parseInt(values.next().toString());
+					callCount++;
+					totalTime += time;
+					weight += weightEvaluate(time);
+				} catch (Exception e) {
+					e.printStackTrace();
+					continue;
+				}
+			}
+			StringBuilder keyBuilder = new StringBuilder();
+			keyBuilder.append(key.toString()).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(Integer.toString(callCount)).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(Integer.toString(totalTime)).append(KQConst.COMMON_SPLIT);
+			keyBuilder.append(Integer.toString(weight));
+		}
+	}
 
 	@Override
 	public void configJob(JobConf conf) {
-		// TODO Auto-generated method stub
+		conf.setMapOutputKeyClass(Text.class);
+		conf.setMapOutputValueClass(Text.class);
 
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Class<? extends Mapper> getMapper() {
-		// TODO Auto-generated method stub
-		return null;
+		return TopMapper.class;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Class<? extends Reducer> getReducer() {
-		// TODO Auto-generated method stub
-		return null;
+		return TopReducer.class;
 	}
 
 }
