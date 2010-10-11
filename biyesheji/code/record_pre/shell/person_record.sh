1a83f2e8e564e4683970cc3d67d0b871e9a2e906
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-11 06:59:45 +0000

    PR shell  xml

---
+++ biyesheji/code/record_pre/shell/person_record.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 1 ];
+then
+    export jobconf_file=$1
+fi
+
+class_root=edu.zju.cs.ooobgy.mr
+laucher=$class_root.common.HadoopLauncher
+job_class=$class_root.jobs.PersonRecord
+jar_path=../dist/record_base_mr.jar
+hadoop_cmd=hadoop
+
+out_path=/group/taobao/ooobgy/call_record/person_record
+$hadoop_cmd fs -rmr $out_path
+$hadoop_cmd jar $jar_path $laucher $job_class $jobconf_file
