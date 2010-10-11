4a8d88d977dcb0a0269647b45f63b95bfc9776e8
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-10-11 06:47:50 +0000

    个人记录mr

--- trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/jobs/PersonRecord.java
+++ trunk/biyesheji/code/record_pre/src/edu/zju/cs/ooobgy/mr/jobs/PersonRecord.java
@@ -1,7 +1,9 @@
 package edu.zju.cs.ooobgy.mr.jobs;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -18,7 +20,7 @@ import edu.zju.cs.ooobgy.mr.common.HadoopJob;
 /**
  * 归一化单个PersonNode的信息 
  * 数据format：
- * PersonId^A主叫次数^A主叫时间^A主叫人数^A被叫次数^A被叫时间^A被叫人数^A通信人数^A双向联系人数^A综合权值 
+ * PersonId^A主叫次数^A主叫时间^A主叫人数^A被叫次数^A被叫时间^A被叫人数^A双向联系人数^A综合权值 
  * 权值公式 weight = (主叫权值/10+主叫人数*50)*1 + (被叫权值/10+被叫人数*50)*0.8 
  * 向下取整
  * 
@@ -32,12 +34,12 @@ public class PersonRecord implements HadoopJob {
 	 * 
 	 * @return weight
 	 */
-	public static int weightEvaluate(int callPcnt, int callWeight, int recPcnt,
-			int recWeight) {
+	public static int weightEvaluate(int callPcnt, int callWeight, int receivePcount,
+			int receiveWeight) {
 		double weight;
 
 		weight = (callWeight / 10.0 + callPcnt * 50) * 1.0
-				+ (recWeight / 10.0 + recPcnt * 50) * 0.8;
+				+ (receiveWeight / 10.0 + receivePcount * 50) * 0.8;
 
 		return (int) weight;
 	}
@@ -97,28 +99,90 @@ public class PersonRecord implements HadoopJob {
 		public void reduce(Text key, Iterator<Text> values,
 				OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
+			Set<String> pubBuff = new HashSet<String>();//求交双向联系的集合buff
+			
+			int callCount = 0;
+			int callTime = 0;
+			int callPcnt = 0;
+			int callWeight = 0;
+			
+			int receiveCount = 0;
+			int receiveTime = 0;
+			int receivePcnt = 0;
+			int receiveWeight = 0;
+			
+			int twoWayCnt = 0;//双向通信人数
+			
+			String line;
+			while (values.hasNext()) {
+				line = values.next().toString();
+				String[] items = line.split(KQConst.COMMON_SPLIT);
+				String flag = items[0];
+				String target = items[1];
+				String count = items[2];
+				String time = items[3];
+				String weight = items[4];
+				if (flag.equals("0")) {//主叫
+					callCount += Integer.parseInt(count);
+					callPcnt ++;
+					callTime += Integer.parseInt(time);
+					callWeight += Integer.parseInt(weight);
+					if (pubBuff.contains(target)) {
+						twoWayCnt ++;
+						pubBuff.remove(target);
+					} else {
+						pubBuff.add(target);
+					}
+				}else if (flag.equals("1")) {//被叫
+					receiveCount += Integer.parseInt(count);
+					receivePcnt ++;
+					receiveTime += Integer.parseInt(time);
+					receiveWeight += Integer.parseInt(weight);
+					if (pubBuff.contains(target)) {
+						twoWayCnt ++;
+						pubBuff.remove(target);
+					} else {
+						pubBuff.add(target);
+					}
+				}
+			}
+			
+			int totalWeight = weightEvaluate(callPcnt, callWeight, receivePcnt, receiveWeight);
+			
+			StringBuilder outLine = new StringBuilder();
+			outLine.append(key.toString()).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(callCount)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(callTime)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(callPcnt)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(receiveCount)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(receiveTime)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(receivePcnt)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(twoWayCnt)).append(KQConst.COMMON_SPLIT);
+			outLine.append(Integer.toString(totalWeight));
+			output.collect(new Text(outLine.toString()), null);
 		}
 }
 	
 	
 	@Override
 	public void configJob(JobConf conf) {
-		// TODO Auto-generated method stub
+		conf.setMapOutputKeyClass(Text.class);
+		conf.setMapOutputValueClass(Text.class);
 
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public Class<? extends Mapper> getMapper() {
-		// TODO Auto-generated method stub
-		return null;
+		return TopMapper.class;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public Class<? extends Reducer> getReducer() {
-		// TODO Auto-generated method stub
-		return null;
+		return TopReducer.class;
 	}
 
 }
