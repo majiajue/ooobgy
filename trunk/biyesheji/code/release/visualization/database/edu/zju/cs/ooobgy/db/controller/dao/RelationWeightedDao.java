da37dcaa00cba979f3537043f2d9adc7baf0a9be
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-11-29 03:55:35 +0000

    数据库操作单独提取出来

--- trunk/biyesheji/code/release/visualization/database/edu/zju/cs/ooobgy/db/controller/dao/RelationWeightedDao.java
+++
@@ -1,7 +0,0 @@
-package edu.zju.cs.ooobgy.db.controller.dao;
-
-import edu.zju.cs.ooobgy.db.entity.RelationWeighted;
-
-public interface RelationWeightedDao extends TDataDao<RelationWeighted,Integer>{
-
-}
