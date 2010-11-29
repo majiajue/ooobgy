0008bae2dcfaadbd51ea22246c793694bca45cd7
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-11-29 03:07:39 +0000

    测试程序变更

--- trunk/biyesheji/code/divisive_algo/src/edu/zju/cs/ooobgy/div1/AdpDevisive.java
+++ trunk/biyesheji/code/divisive_algo/src/edu/zju/cs/ooobgy/div1/AdpDevisive.java
@@ -7,10 +7,10 @@ public class AdpDevisive {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DataAdapter adapter = new DataAdapter();
-		adapter.Csv2DB("data/karate.csv");
+//		DataAdapter adapter = new DataAdapter();
+//		adapter.Csv2DB("data/karate.csv");
 		String shareFile = "data/katate.net";
-		adapter.EdgeDB2JungFile(shareFile);
+//		adapter.EdgeDB2JungFile(shareFile);
 		ClusterPlatform platform = new ClusterPlatform();
 		platform.runSharedPlatForm(shareFile);
 	}
