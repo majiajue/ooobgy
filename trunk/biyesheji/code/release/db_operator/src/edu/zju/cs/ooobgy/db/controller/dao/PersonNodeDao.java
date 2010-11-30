d480316fd7d655ec6a4dbcbe740bcfc40fdb57fa
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-11-30 09:06:35 +0000

    类注释

--- trunk/biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/controller/dao/PersonNodeDao.java
+++ trunk/biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/controller/dao/PersonNodeDao.java
@@ -2,5 +2,11 @@ package edu.zju.cs.ooobgy.db.controller.dao;
 
 import edu.zju.cs.ooobgy.db.entity.PersonNode;
 
+/**
+ * 
+ * @author frogcherry 周晓龙
+ * @created 2010-11-30
+ * @Email frogcherry@gmail.com
+ */
 public interface PersonNodeDao extends TDataDao<PersonNode,Integer> {
 }
