ebd7a161b013e9e8eb3d17c064ca1106d1ca2f25
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-09 08:35:29 +0000

    矩阵实现

---
+++ trunk/biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/math/Matrix.java
@@ -0,0 +1,11 @@
+package edu.zju.cs.ooobgy.algo.math;
+
+/**
+ * 描述矩阵的数据结构的封装，仅实现现在需要的功能
+ * @author frogcherry 周晓龙
+ * @created 2010-12-9
+ * @Email frogcherry@gmail.com
+ */
+public class Matrix {
+	
+}
