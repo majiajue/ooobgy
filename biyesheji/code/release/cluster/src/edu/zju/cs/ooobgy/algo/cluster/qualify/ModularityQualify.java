6e7031168c71d5e6b2b9d4873e7a7c10d1ca1a82
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-12 09:11:05 +0000

    实现了聚类评分算法

--- biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/qualify/ModularityQualify.java
+++ biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/qualify/ModularityQualify.java
@@ -1,8 +1,13 @@
 package edu.zju.cs.ooobgy.algo.cluster.qualify;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections15.Transformer;
+import org.apache.commons.collections15.functors.ConstantTransformer;
+
+import edu.zju.cs.ooobgy.algo.math.Matrix;
 import edu.zju.cs.ooobgy.graph.WeakComponentGraph;
 import edu.zju.cs.ooobgy.graph.util.Pair;
 
@@ -15,16 +20,65 @@ import edu.zju.cs.ooobgy.graph.util.Pair;
  * @see "M. E. J. Newman and M. Girvan: Finding and evaluating community structure in networks (2004)"
  */
 public class ModularityQualify<V, E> implements ClusterQualify<V, E>{
+	private Map<E, Pair<V>> originEdges;
+	private Transformer<E, ? extends Number> edge_weights;
 
-	@Override
-	public double qualify(WeakComponentGraph<V, E> graph, Map<E, Pair<V>> originEdges) {
-		return qualify(graph.getComponents(), originEdges);
+	/**
+	 * 对于Modularity Qualify评价算法，必须还要已知原始的图结构，可选传入边的权重
+	 * @param originEdges
+	 * @param edge_weights
+	 */
+	public ModularityQualify(Map<E, Pair<V>> originEdges,
+			Transformer<E, ? extends Number> edge_weights) {
+		super();
+		this.originEdges = originEdges;
+		this.edge_weights = edge_weights;
+	}
+
+	/**
+	 * 对于Modularity Qualify评价算法，必须还要已知原始的图结构，可选传入边的权重
+	 * @param originEdges
+	 * @param edge_weights
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ModularityQualify(Map<E, Pair<V>> originEdges) {
+		super();
+		this.originEdges = originEdges;
+		this.edge_weights = new ConstantTransformer(1);
 	}
 
 	@Override
-	public double qualify(Set<Set<V>> clusters, Map<E, Pair<V>> originEdges) {
+	public double qualify(WeakComponentGraph<V, E> graph) {
+		return qualify(graph.getComponents());
+	}
+
+	@Override
+	public double qualify(Set<Set<V>> clusters) {
+		//1.生成判定矩阵
+		int clusterCount = clusters.size();
+		Matrix modulMatrix = new Matrix(clusterCount, clusterCount);
+		Map<V, Integer> vertexClusterMap = mapVertices(clusters);
+		int edgeCount = originEdges.size();
+		
+		for (E e : originEdges.keySet()) {
+			
+		}
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	private Map<V, Integer> mapVertices(Set<Set<V>> clusters) {
+		//50% hash填充率，加速
+		Map<V, Integer> vertexClusterMap = new HashMap<V, Integer>(clusters.size()*2);
+		Integer cid = 0;
+		for (Set<V> cluster : clusters) {
+			for (V v : cluster) {
+				vertexClusterMap.put(v, cid);
+			}
+			cid++;
+		}
+		
+		return vertexClusterMap;
 	}
 
 }
