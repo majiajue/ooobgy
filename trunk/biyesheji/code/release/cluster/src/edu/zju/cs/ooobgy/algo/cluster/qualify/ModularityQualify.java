cb3db2f5cec010a4847017e718f187394c0e4d40
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-14 03:28:23 +0000

    Modularity Qualify评价算法完成

--- trunk/biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/qualify/ModularityQualify.java
+++ trunk/biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/qualify/ModularityQualify.java
@@ -55,18 +55,50 @@ public class ModularityQualify<V, E> implements ClusterQualify<V, E>{
 	@Override
 	public double qualify(Set<Set<V>> clusters) {
 		//1.生成判定矩阵
-		int clusterCount = clusters.size();
-		Matrix modulMatrix = new Matrix(clusterCount, clusterCount);
-		Map<V, Integer> vertexClusterMap = mapVertices(clusters);
-		int edgeCount = originEdges.size();
-		
-		for (E e : originEdges.keySet()) {
-			
+		Matrix moduleMatrix = genQMatrix(clusters);
+		//2.计算quality值
+		double quality = 0;
+		for (int i = 0; i < clusters.size(); i++) {
+			quality += moduleMatrix.element(i, i);
+			double ai = moduleMatrix.sumRow(i);
+			quality -= ai*ai;
 		}
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return quality;
 	}
 
+	/**
+	 * 生成判定矩阵
+	 * @param clusters
+	 * @return
+	 */
+	private Matrix genQMatrix(Set<Set<V>> clusters) {
+		int clusterCount = clusters.size();
+		Matrix moduleMatrix = new Matrix(clusterCount, clusterCount);
+		Map<V, Integer> vertexClusterMap = mapVertices(clusters);
+		
+		double weightSum = 0;
+		for (E e : originEdges.keySet()){
+			weightSum += edge_weights.transform(e).doubleValue();
+		}
+		
+		for (E e : originEdges.keySet()) {
+			Pair<V> vertices = originEdges.get(e);
+			Integer v1Cid = vertexClusterMap.get(vertices.getFirst());
+			Integer v2Cid = vertexClusterMap.get(vertices.getSecond());
+			double addi = edge_weights.transform(e).doubleValue() / weightSum;
+			moduleMatrix.addElement(v1Cid, v2Cid, addi);
+			moduleMatrix.addElement(v2Cid, v1Cid, addi);
+		}
+		
+		return moduleMatrix;
+	}
+
+	/**
+	 * 建立点与cluster的配对
+	 * @param clusters
+	 * @return
+	 */
 	private Map<V, Integer> mapVertices(Set<Set<V>> clusters) {
 		//50% hash填充率，加速
 		Map<V, Integer> vertexClusterMap = new HashMap<V, Integer>(clusters.size()*2);
