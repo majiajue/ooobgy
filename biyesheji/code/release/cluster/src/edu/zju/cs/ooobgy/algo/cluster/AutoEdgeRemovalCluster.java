d0027f58be261969865de6bf659db9c7649cbbe2
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-14 06:52:04 +0000

    自动切边完成

--- biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/AutoEdgeRemovalCluster.java
+++ biyesheji/code/release/cluster/src/edu/zju/cs/ooobgy/algo/cluster/AutoEdgeRemovalCluster.java
@@ -6,6 +6,12 @@ import org.apache.commons.collections15.Transformer;
 
 import edu.zju.cs.ooobgy.graph.Graph;
 
+/**
+ * 自动切边算法接口
+ * @author frogcherry 周晓龙
+ * @created 2010-12-14
+ * @Email frogcherry@gmail.com
+ */
 public interface AutoEdgeRemovalCluster<V,E> extends Transformer<Graph<V,E>,Set<Set<V>>>{
 
 }
