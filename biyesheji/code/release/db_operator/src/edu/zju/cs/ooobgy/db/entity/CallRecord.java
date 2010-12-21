9e0a9c077c51036562b9db4e77339e5323d90311
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-21 07:31:16 +0000

    相关bean重置

--- biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/entity/CallRecord.java
+++ biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/entity/CallRecord.java
@@ -1,63 +1,14 @@
-/**
- * @Author 周晓龙   frogcherry
- * @Email frogcherry@gmail.com
- * @CreateDate 2010-9-7
- */
-
 package edu.zju.cs.ooobgy.db.entity;
 
 import java.sql.Timestamp;
 
 public class CallRecord {
-	private Integer cid;
-	private String callerNum;
-	private String receiverNum;
-	private Timestamp startTime;
-	private String stationId;
-	private String stationCode;
-	private Double callDuration;
-	public String getCallerNum() {
-		return callerNum;
+	private Integer callId;
+	public Integer getCallId() {
+		return callId;
 	}
-	public void setCallerNum(String callerNum) {
-		this.callerNum = callerNum;
+	public void setCallId(Integer callId) {
+		this.callId = callId;
 	}
-	public String getReceiverNum() {
-		return receiverNum;
-	}
-	public void setReceiverNum(String receiverNum) {
-		this.receiverNum = receiverNum;
-	}
-	public Timestamp getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(Timestamp startTime) {
-		this.startTime = startTime;
-	}
-	public String getStationId() {
-		return stationId;
-	}
-	public void setStationId(String stationId) {
-		this.stationId = stationId;
-	}
-	public String getStationCode() {
-		return stationCode;
-	}
-	public void setStationCode(String stationCode) {
-		this.stationCode = stationCode;
-	}
-	public Double getCallDuration() {
-		return callDuration;
-	}
-	public void setCallDuration(Double callDuration) {
-		this.callDuration = callDuration;
-	}
-	public Integer getCid() {
-		return cid;
-	}
-	public void setCid(Integer cid) {
-		this.cid = cid;
-	}
-	
 	
 }
