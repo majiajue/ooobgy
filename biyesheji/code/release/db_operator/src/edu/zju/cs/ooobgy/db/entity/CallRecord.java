dc73bfadca6e60fc58e2d3af85b0a47694779580
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-21 07:37:10 +0000

    基础通话数据pojo

--- biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/entity/CallRecord.java
+++ biyesheji/code/release/db_operator/src/edu/zju/cs/ooobgy/db/entity/CallRecord.java
@@ -2,13 +2,61 @@ package edu.zju.cs.ooobgy.db.entity;
 
 import java.sql.Timestamp;
 
+/**
+ * 基础通话数据pojo
+ * @author frogcherry 周晓龙
+ * @created 2010-12-21
+ * @Email frogcherry@gmail.com
+ */
 public class CallRecord {
 	private Integer callId;
+	private String caller;
+	private String receiver;
+	private Timestamp stime;
+	private Integer duringTime;
+	private String stationId;
+	private String stationCode;
+	
 	public Integer getCallId() {
 		return callId;
 	}
 	public void setCallId(Integer callId) {
 		this.callId = callId;
 	}
-	
+	public String getCaller() {
+		return caller;
+	}
+	public void setCaller(String caller) {
+		this.caller = caller;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public Timestamp getStime() {
+		return stime;
+	}
+	public void setStime(Timestamp stime) {
+		this.stime = stime;
+	}
+	public Integer getDuringTime() {
+		return duringTime;
+	}
+	public void setDuringTime(Integer duringTime) {
+		this.duringTime = duringTime;
+	}
+	public String getStationId() {
+		return stationId;
+	}
+	public void setStationId(String stationId) {
+		this.stationId = stationId;
+	}
+	public String getStationCode() {
+		return stationCode;
+	}
+	public void setStationCode(String stationCode) {
+		this.stationCode = stationCode;
+	}
 }
