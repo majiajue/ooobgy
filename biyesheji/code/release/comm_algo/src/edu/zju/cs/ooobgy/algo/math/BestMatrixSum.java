5d9a0fb615babe140bd958b04f338ccf8a094db9
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-22 08:23:51 +0000

    矩阵转置

---
+++ biyesheji/code/release/comm_algo/src/edu/zju/cs/ooobgy/algo/math/BestMatrixSum.java
@@ -0,0 +1,11 @@
+package edu.zju.cs.ooobgy.algo.math;
+
+/**
+ * 计算矩阵中一些最优组合的方法，这里主要是一些最优元素和的方法
+ * @author frogcherry 周晓龙
+ * @created 2010-12-22
+ * @Email frogcherry@gmail.com
+ */
+public class BestMatrixSum {
+	
+}
--- biyesheji/code/release/comm_algo/test/edu/zju/cs/ooobgy/algo/math/MatrixTest.java
+++ biyesheji/code/release/comm_algo/test/edu/zju/cs/ooobgy/algo/math/MatrixTest.java
@@ -1,8 +1,18 @@
 package edu.zju.cs.ooobgy.algo.math;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
+/**
+ * 
+ * @author frogcherry 周晓龙
+ * @created 2010-12-12
+ * @Email frogcherry@gmail.com
+ */
 public class MatrixTest extends TestCase{
+	
+	@Test
 	public void testMatrix(){
 		Matrix matrix = new Matrix(3, 3);
 		for (int i = 0; i < 3; i++) {
@@ -15,5 +25,15 @@ public class MatrixTest extends TestCase{
 		System.out.println("-------------------------");
 		matrix.addElement(1, 1, 0.3);
 		System.out.println(matrix);
+	}
+	
+	@Test
+	public void testTranspose(){
+		double[][] old = {{1,2,3,4},{11,22,33,44},{111,222,333,444}};
+		Matrix matrix = new Matrix(old, 3, 4);
+		System.out.println("matrix:");
+		System.out.println(matrix);
+		System.out.println("Transposed matrix:");
+		System.out.println(matrix.transpose());
 	}
 }
