9ac0c211f6e92ffcbccf59c17046751d603c3450
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-22 08:57:10 +0000

    最优矩阵计算

--- biyesheji/code/release/comm_algo/src/edu/zju/cs/ooobgy/algo/math/Matrix.java
+++ biyesheji/code/release/comm_algo/src/edu/zju/cs/ooobgy/algo/math/Matrix.java
@@ -2,7 +2,8 @@ package edu.zju.cs.ooobgy.algo.math;
 
 /**
  * 描述矩阵的数据结构的封装，仅实现现在需要的功能
- * 内容元素使用double基本类型填充，暂时不需要复杂的封装实现
+ * 内容元素使用double基本类型填充，暂时不需要复杂的封装实现，
+ * 如在其他场合使用，请转化为double数值矩阵
  * @author frogcherry 周晓龙
  * @created 2010-12-9
  * @Email frogcherry@gmail.com
