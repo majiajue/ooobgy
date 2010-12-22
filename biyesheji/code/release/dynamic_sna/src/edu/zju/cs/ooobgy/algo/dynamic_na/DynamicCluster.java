132371f62bcbe371bed36bdf48e2b938af16f5f7
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2010-12-22 10:45:40 +0000

    相似度计算

--- biyesheji/code/release/dynamic_sna/src/edu/zju/cs/ooobgy/algo/dynamic_na/DynamicCluster.java
+++ biyesheji/code/release/dynamic_sna/src/edu/zju/cs/ooobgy/algo/dynamic_na/DynamicCluster.java
@@ -1,24 +1,30 @@
 package edu.zju.cs.ooobgy.algo.dynamic_na;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.commons.collections15.Transformer;
 
 import edu.zju.cs.ooobgy.algo.dynamic_na.pojo.ClusterSlice;
+import edu.zju.cs.ooobgy.algo.math.Matrix;
 
 /**
  * 对两个切片进行动态分析
+ * 
  * @author frogcherry 周晓龙
  * @created 2010-12-22
  * @Email frogcherry@gmail.com
  */
-public class DynamicCluster<V,E> implements Transformer<ClusterSlice<V, E>, ClusterSlice<V, E>>{
-	private ClusterSlice<V, E> preSlice;//已知的上一切片
-	private Set<Set<V>> newSliceClusters;//已知的待处理切片的团伙信息
-	
+public class DynamicCluster<V, E> implements
+		Transformer<ClusterSlice<V, E>, ClusterSlice<V, E>> {
+	private ClusterSlice<V, E> preSlice;// 已知的上一切片
+	private Set<Set<V>> newSliceClusters;// 已知的待处理切片的团伙信息
+
 	/**
-	 * 使用已知的上一切片，已知的待处理切片的团伙信息进行构造;
-	 * 已知条件不满足无法进行分析
+	 * 使用已知的上一切片，已知的待处理切片的团伙信息进行构造; 已知条件不满足无法进行分析
+	 * 
 	 * @param preSlice
 	 * @param newSliceClusters
 	 */
@@ -29,14 +35,17 @@ public class DynamicCluster<V,E> implements Transformer<ClusterSlice<V, E>, Clus
 		this.newSliceClusters = newSliceClusters;
 	}
 
-
-
 	/**
 	 * 传入新的切片（包含图信息，不包含团伙对应信息）,根据已知的上一切片进行对应动态分析
 	 */
 	@Override
 	public ClusterSlice<V, E> transform(ClusterSlice<V, E> newSlice) {
 		ClusterSlice<V, E> slice = newSlice;
+		List<Entry<String, Set<V>>> preCluster = new ArrayList<Entry<String, Set<V>>>(
+				preSlice.getClusters().entrySet());//已知团伙的id对应情况，转为list方便后续分析
+		List<Set<V>> newClusters = new ArrayList<Set<V>>(newSliceClusters);//新的团伙信息，待分析list
+		Matrix qualifyMatrix = new Matrix(preCluster.size(), newClusters.size());
+		
 		// TODO Auto-generated method stub
 		return slice;
 	}
---
+++ biyesheji/code/release/dynamic_sna/src/edu/zju/cs/ooobgy/algo/dynamic_na/qualify/ClusterSimilarity.java
@@ -0,0 +1,41 @@
+package edu.zju.cs.ooobgy.algo.dynamic_na.qualify;
+
+import java.util.Set;
+
+import org.apache.commons.collections15.Transformer;
+
+/**
+ * 计算两个团伙C1,C2的相似度 = |C1 交 C@| / |C1 并 C2| </br>
+ * 先已知C1,然后传入C2进行计算
+ * @author frogcherry 周晓龙
+ * @created 2010-12-18
+ * @Email frogcherry@gmail.com
+ */
+public class ClusterSimilarity<V> implements Transformer<Set<V>, Double>{
+	private Set<V> c1;
+	
+	public ClusterSimilarity(Set<V> c1) {
+		super();
+		this.c1 = c1;
+	}
+
+	/**
+	 * 计算两个团伙C1,C2的相似度 = |C1 交 C@| / |C1 并 C2| </br>
+	 * 先已知C1,然后传入C2进行计算
+	 */
+	@Override
+	public Double transform(Set<V> c2) {
+		int intersectionSize = 0;//交集的大小
+		int unionSize = 0;//并集的大小
+		
+		for (V vertex : c1) {
+			if (c2.contains(vertex)) {
+				intersectionSize ++;
+			}
+		}
+		unionSize = c1.size() + c2.size() - intersectionSize;
+		
+		return new Double(unionSize) / new Double(intersectionSize);
+	}
+
+}
