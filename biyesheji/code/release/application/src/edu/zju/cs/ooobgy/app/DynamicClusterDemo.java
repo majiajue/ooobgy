ddb37101f397214f405af8378783639a21d7dafc
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-02-25 03:00:41 +0000

    切片的demo

---
+++ biyesheji/code/release/application/src/edu/zju/cs/ooobgy/app/DynamicClusterDemo.java
@@ -0,0 +1,42 @@
+package edu.zju.cs.ooobgy.app;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import edu.zju.cs.ooobgy.app.platform.TimeSliceClusterPlatform;
+
+@SuppressWarnings("serial")
+public class DynamicClusterDemo extends JApplet implements Runnable{
+
+	@Override
+	public void run() {
+		TimeSliceClusterPlatform platformLeft = new TimeSliceClusterPlatform();
+		TimeSliceClusterPlatform platformRight = new TimeSliceClusterPlatform();
+		try {
+			JPanel slice1 = platformLeft.setUpView("001001");
+			JPanel slice2 = platformRight.setUpView("001001");
+			Container content = getContentPane();
+			content.setLayout(new BorderLayout());
+            content.add(slice1,BorderLayout.WEST);
+            content.add(slice2,BorderLayout.EAST);
+            
+            JFrame jf = new JFrame();
+            jf.setSize(1024, 768);
+    		jf.getContentPane().add(this);
+    		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    		jf.pack();
+    		jf.setVisible(true);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DynamicClusterDemo demo = new DynamicClusterDemo();
+		demo.run();
+	}
+	
+}
