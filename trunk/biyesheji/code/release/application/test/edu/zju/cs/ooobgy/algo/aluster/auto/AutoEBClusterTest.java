8806b60bd91c5bc91040cf136fe1e97e2a1a58c7
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-04-11 09:23:18 +0000

    MQ还是有问题啊，抗不住

--- trunk/biyesheji/code/release/application/test/edu/zju/cs/ooobgy/algo/aluster/auto/AutoEBClusterTest.java
+++ trunk/biyesheji/code/release/application/test/edu/zju/cs/ooobgy/algo/aluster/auto/AutoEBClusterTest.java
@@ -1,10 +1,12 @@
 package edu.zju.cs.ooobgy.algo.aluster.auto;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
 
 import edu.zju.cs.ooobgy.algo.cluster.auto.AutoEdgeBetwennessCluster;
+import edu.zju.cs.ooobgy.algo.cluster.qualify.NMIQualify;
 import edu.zju.cs.ooobgy.dt.db.ClusterGraphDBLoader;
 import edu.zju.cs.ooobgy.graph.ClusterGraph;
 
@@ -18,12 +20,54 @@ public class AutoEBClusterTest extends TestCase {
 		ClusterGraph<String, Integer> graph = dbLoader.load("001001");
 		System.out.println("==========before cluster==========");
 		System.out.println(graph);
-		System.out.println("==========after cluster==========");
+		System.out.println("==========clustering==========");
 		AutoEdgeBetwennessCluster<String, Integer> autoCluster = new AutoEdgeBetwennessCluster<String, Integer>(
 				graph.getEdgeWeights());
 		Set<Set<String>> groups = autoCluster.autoCluster(graph);
+		System.out.println("==========after cluster==========");
 		System.out.println(graph);
 		System.out.println("----------groups--------");
 		System.out.println(groups);
+		System.out.println("----------NMI--------");
+		Set<Set<String>> realClsters = makeSet(new String[][]{{"1","2","3"},{"4","5","6","7"}});
+		System.out.println("real cluster:" + realClsters);
+		NMIQualify qualify = new NMIQualify<String>();
+		double NMI = qualify.qualifyNMI(realClsters, groups);
+		System.out.println("NMI = " + NMI);
+	}
+	
+	@Test
+	public void testCase2() {
+		ClusterGraphDBLoader dbLoader = new ClusterGraphDBLoader();
+		ClusterGraph<String, Integer> graph = dbLoader.load("201002");
+		System.out.println("==========before cluster==========");
+		System.out.println(graph);
+		System.out.println("==========clustering==========");
+		AutoEdgeBetwennessCluster<String, Integer> autoCluster = new AutoEdgeBetwennessCluster<String, Integer>(
+				graph.getEdgeWeights());
+		Set<Set<String>> groups = autoCluster.autoCluster(graph);
+		System.out.println("==========after cluster==========");
+		System.out.println(graph);
+		System.out.println("----------groups--------");
+		System.out.println(groups);
+		System.out.println("----------NMI--------");
+		Set<Set<String>> realClsters = makeSet(new String[][]{{"1","2","3"},{"4","5","6","7"}});
+		System.out.println("real cluster:" + realClsters);
+		NMIQualify qualify = new NMIQualify<String>();
+		double NMI = qualify.qualifyNMI(realClsters, groups);
+		System.out.println("NMI = " + NMI);
+	}
+	
+	private Set<Set<String>> makeSet(String[][] cm) {
+		Set<Set<String>> cs = new HashSet<Set<String>>();
+		for (String[] c : cm) {
+			Set<String> cc = new HashSet<String>();
+			for (String v : c) {
+				cc.add(v);
+			}
+			cs.add(cc);
+		}
+		
+		return cs;
 	}
 }
