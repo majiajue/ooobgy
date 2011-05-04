7f28c65150cff2c232a96e1b748dbfb49fbd0e94
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-05-04 11:15:47 +0000

    动态分析节点和团伙

---
+++ biyesheji/code/release/comm_conf/src/edu/zju/cs/ooobgy/app/cache/DCD_Cache.java
@@ -0,0 +1,29 @@
+package edu.zju.cs.ooobgy.app.cache;
+
+import java.awt.Color;
+/**
+ * 公共数据区
+ * @author frogcherry 周晓龙
+ * @created 2011-5-4
+ * @Email frogcherry@gmail.com
+ */
+public class DCD_Cache {
+	//1.状态配置
+	public static Boolean clusterMapped = false;
+	//2.持久数据
+	public static final Color[] similarColors =
+	{
+		new Color(216, 134, 134),
+		new Color(135, 137, 211),
+		new Color(134, 206, 189),
+		new Color(206, 176, 134),
+		new Color(194, 204, 134),
+		new Color(145, 214, 134),
+		new Color(133, 178, 209),
+		new Color(103, 148, 255),
+		new Color(60, 220, 220),
+		new Color(30, 250, 100)
+	};
+	//3.数据cache
+	
+}
