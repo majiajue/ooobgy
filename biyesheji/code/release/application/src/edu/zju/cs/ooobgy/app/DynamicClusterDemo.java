4d99b21fec87f4869cb878d72af156f57fa72a97
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-05-04 11:14:50 +0000

    动态分析节点和团伙

--- biyesheji/code/release/application/src/edu/zju/cs/ooobgy/app/DynamicClusterDemo.java
+++ biyesheji/code/release/application/src/edu/zju/cs/ooobgy/app/DynamicClusterDemo.java
@@ -2,22 +2,30 @@ package edu.zju.cs.ooobgy.app;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
+import edu.zju.cs.ooobgy.app.action.DynamicClusterAction;
 import edu.zju.cs.ooobgy.app.platform.TimeSliceClusterPlatform;
 
 @SuppressWarnings("serial")
 public class DynamicClusterDemo extends JApplet implements Runnable{
-
-	@Override
-	public void run() {
-		TimeSliceClusterPlatform platformLeft = new TimeSliceClusterPlatform();
-		TimeSliceClusterPlatform platformRight = new TimeSliceClusterPlatform();
+	private TimeSliceClusterPlatform leftPlatform;
+	private TimeSliceClusterPlatform rightPlatform;
+	
+	public void setUpView(){
+		leftPlatform = new TimeSliceClusterPlatform();
+		rightPlatform = new TimeSliceClusterPlatform();
 		try {
-			JPanel slice1 = platformLeft.setUpView("001101");
-			JPanel slice2 = platformRight.setUpView("001102");
+			JPanel slice1 = leftPlatform.setUpView("001101");
+			JPanel slice2 = rightPlatform.setUpView("001102");
 			Container content = getContentPane();
 			content.setLayout(new BorderLayout());
             content.add(slice1,BorderLayout.WEST);
@@ -26,12 +34,70 @@ public class DynamicClusterDemo extends JApplet implements Runnable{
             JFrame jf = new JFrame();
             jf.setSize(1024, 768);
     		jf.getContentPane().add(this);
+            //jf.setJMenuBar(setUpSwingMenuBar());
+    		jf.setMenuBar(setUpAwtMenuBar());
     		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     		jf.pack();
     		jf.setVisible(true);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * AWT菜单，解决视图覆盖问题
+	 * @return
+	 */
+	public MenuBar setUpAwtMenuBar(){
+		MenuBar menuBar = new MenuBar();
+		DynamicClusterAction actionListener = new DynamicClusterAction(leftPlatform,rightPlatform);
+		
+		Menu fenxiMenu;
+		fenxiMenu = new Menu("Dynamic Analysis");
+		menuBar.add(fenxiMenu);
+		MenuItem zongtiItem = new MenuItem("DA_all");
+		zongtiItem.setActionCommand("DA_all");//DA:dynamic analysis
+		zongtiItem.addActionListener(actionListener);
+		fenxiMenu.add(zongtiItem);
+		MenuItem nodeItem = new MenuItem("DA_node");
+		nodeItem.setActionCommand("DA_node");
+		nodeItem.addActionListener(actionListener);
+		fenxiMenu.add(nodeItem);
+		MenuItem clusterItem = new MenuItem("DA_cluster");
+		clusterItem.setActionCommand("DA_cluster");
+		clusterItem.addActionListener(actionListener);
+		fenxiMenu.add(clusterItem);
+		
+		return menuBar;
+	}
+	
+	/**
+	 * Swing菜单
+	 * @return
+	 */
+	public JMenuBar setUpSwingMenuBar() {
+		DynamicClusterAction actionListener = new DynamicClusterAction(leftPlatform,rightPlatform);
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fenxiMenu = new JMenu("演化分析");
+		menuBar.add(fenxiMenu);
+		JMenuItem allItem = new JMenuItem("总体分析");
+		allItem.addActionListener(actionListener);
+		fenxiMenu.add(allItem);
+		JMenuItem nodeItem = new JMenuItem("节点行为");
+		nodeItem.addActionListener(actionListener);
+		fenxiMenu.add(nodeItem);
+		JMenuItem clusterItem = new JMenuItem("团伙行为");
+		clusterItem.addActionListener(actionListener);
+		fenxiMenu.add(clusterItem);
+		
+		
+		return menuBar;
+	}
+
+	@Override
+	public void run() {
+		setUpView();
 	}
 	
 	public static void main(String[] args) {
---
+++ biyesheji/code/release/application/src/edu/zju/cs/ooobgy/app/action/DynamicClusterAction.java
@@ -0,0 +1,75 @@
+package edu.zju.cs.ooobgy.app.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
+
+import edu.zju.cs.ooobgy.algo.dynamic_na.ClusterSliceMapper;
+import edu.zju.cs.ooobgy.algo.dynamic_na.analayzer.NodeEventAnalyzer;
+import edu.zju.cs.ooobgy.algo.dynamic_na.event.NodeEvent;
+import edu.zju.cs.ooobgy.algo.dynamic_na.pojo.ClusterSlice;
+import edu.zju.cs.ooobgy.app.cache.DCD_Cache;
+import edu.zju.cs.ooobgy.app.platform.TimeSliceClusterPlatform;
+
+public class DynamicClusterAction implements ActionListener {
+	private TimeSliceClusterPlatform leftPlatform;
+	private TimeSliceClusterPlatform rightPlatform;
+	private ClusterSlice<String, Integer> preSlice;
+	private ClusterSlice<String, Integer> nowSlice;
+
+	public DynamicClusterAction(TimeSliceClusterPlatform leftPlatform,
+			TimeSliceClusterPlatform rightPlatform) {
+		super();
+		this.leftPlatform = leftPlatform;
+		this.rightPlatform = rightPlatform;
+		preSlice = leftPlatform.getClusterSlice();
+		nowSlice = rightPlatform.getClusterSlice();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if (cmd.equals("DA_all")) {
+			allAction();
+		}else if (cmd.equals("DA_node")) {
+			nodeAction();
+		}else if (cmd.equals("DA_cluster")) {
+			clusterAction();
+		}
+	}
+
+	private void clusterAction() {
+		mapCluster();
+		
+	}
+
+	private void nodeAction() {
+		mapCluster();
+		NodeEventAnalyzer<String, Integer> analyzer = new NodeEventAnalyzer<String, Integer>(preSlice);
+		Set<NodeEvent<String>> nodeEvents = analyzer.analyze(nowSlice);
+		System.out.println("================= Node Events =================");
+		for (NodeEvent<String> nodeEvent : nodeEvents) {
+			System.out.println(nodeEvent);
+		}
+		System.out.println();
+	}
+
+	private void allAction() {
+		mapCluster();
+		
+	}
+	
+	/**
+	 * 映射团伙id，可能需要对rightPlatform中的图进行重绘
+	 */
+	private void mapCluster(){
+		if (!DCD_Cache.clusterMapped) {
+			ClusterSliceMapper<String, Integer> sliceMapper = new ClusterSliceMapper<String, Integer>(preSlice);
+			nowSlice = sliceMapper.transform(nowSlice);
+			rightPlatform.setClusterSlice(nowSlice);
+			rightPlatform.repaintSlice();
+			
+			DCD_Cache.clusterMapped = true;
+		}
+	}
+}
