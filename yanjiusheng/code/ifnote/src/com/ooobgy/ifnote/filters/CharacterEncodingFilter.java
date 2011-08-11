7854106b0d68413e93ef4d05dabb4c6241fb5565
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-08-11 10:59:07 +0000

    加入一些工具类

---
+++ yanjiusheng/code/ifnote/src/com/ooobgy/ifnote/filters/CharacterEncodingFilter.java
@@ -0,0 +1,53 @@
+package com.ooobgy.ifnote.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * 字符编码统一utf-8
+ * @author frogcherry 周晓龙
+ * @created 2011-8-11
+ * @Email frogcherry@gmail.com
+ */
+public class CharacterEncodingFilter implements Filter {
+
+	/*
+	 * 统一传递参数用utf-8
+	 * 
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+	 * javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
