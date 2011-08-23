e2f03b0c05a53aa39b6cc02e628357c52c34be6b
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-08-23 12:45:28 +0000

    扩展bean

---
+++ yanjiusheng/code/ifnote/src/com/ooobgy/ifnote/dispbeans/Disp_Fund.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author : CHE.R.RY (周晓龙)
+ * @Email  : frogcherry@gmail.com
+ * @Date   : 2011-8-23
+ */
+package com.ooobgy.ifnote.dispbeans;
+
+import com.ooobgy.ifnote.entity.Inote_Fund;
+
+public class Disp_Fund extends Inote_Fund {
+
+}
---
+++ yanjiusheng/code/ifnote/src/com/ooobgy/ifnote/dispbeans/Disp_Stock.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author : CHE.R.RY (周晓龙)
+ * @Email  : frogcherry@gmail.com
+ * @Date   : 2011-8-23
+ */
+package com.ooobgy.ifnote.dispbeans;
+
+import com.ooobgy.ifnote.entity.Inote_Stock;
+
+public class Disp_Stock extends Inote_Stock {
+
+}
