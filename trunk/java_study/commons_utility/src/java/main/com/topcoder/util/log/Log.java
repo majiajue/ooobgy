d2bacb82e04395817ff52e19ed3cbc079079c1e2
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-09-25 08:48:32 +0000

    LUH

--- trunk/java_study/commons_utility/src/java/main/com/topcoder/util/log/Log.java
+++ trunk/java_study/commons_utility/src/java/main/com/topcoder/util/log/Log.java
@@ -1,6 +1,11 @@
 package com.topcoder.util.log;
 
 /**
+ * 自定义logger的接口
+ * @author Frogcherry 周晓龙 frogcherry@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
  */
 public interface Log {
+    public void log(Level level, String message);
 }
