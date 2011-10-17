d538212629b903880fefd402766503213c91e5de
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-10-17 13:42:00 +0000

    真难画

---
+++ trunk/java_study/income_tax/src/java/demo/com/ooobgy/demo/TPConst.java
@@ -0,0 +1,10 @@
+package com.ooobgy.demo;
+
+public class TPConst {
+    public static final int SPANEL_WIDTH = 300;
+    public static final int SPANEL_HEIGHT = 600;
+    
+    public static final String TYPE_SALARY = "工资薪金所得（月薪）";
+    public static final String TYPE_ACC = "偶然所得";
+    public static final String TYPE_INTER = "银行利息所得";
+}
---
+++ trunk/java_study/income_tax/src/java/demo/com/ooobgy/demo/TaxPlatform.java
@@ -0,0 +1,67 @@
+package com.ooobgy.demo;
+
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.ooobgy.income.conf.ConfReader;
+import com.ooobgy.income.exception.IllTaxConfException;
+import com.ooobgy.income.pojo.OtherTaxConf;
+import com.ooobgy.income.pojo.SalaryTaxConf;
+
+public class TaxPlatform extends JApplet{
+    private static final long serialVersionUID = 450344345065490509L;
+    private SalaryTaxConf stConf;
+    private OtherTaxConf otConf;
+    private final String defPropFilePath;
+    
+    public TaxPlatform(String defPropFilePath) throws HeadlessException {
+        super();
+        this.defPropFilePath = defPropFilePath;
+    }
+
+    private JPanel setUpView() throws IllTaxConfException, FileNotFoundException, IOException {
+        this.stConf = ConfReader.makeSalaryTaxConf(defPropFilePath);
+        this.otConf = ConfReader.makeOtherTaxConf(defPropFilePath);
+        SinglePanel sp = new SinglePanel(stConf, otConf);
+        JPanel singlePanel = sp.setUpView();
+        
+        JPanel context = new JPanel(new GridLayout(1, 2));
+        context.add(singlePanel);
+        return context;
+    }
+    
+    @Override
+    public void start() {
+        JPanel context;
+        try {
+            context = setUpView();
+            Container content = getContentPane();
+            content.add(context);
+        } catch (IllTaxConfException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        TaxPlatform tf = new TaxPlatform("conf/tax-conf.properties");
+        
+        tf.start();
+        JFrame jf = new JFrame();
+        jf.getContentPane().add(tf);
+        jf.setTitle("ooobgy个税计算器        (单位:元)");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.pack();
+        jf.setVisible(true);
+    }
+}
