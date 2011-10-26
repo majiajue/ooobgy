f10a7904e14d5e654abcfd7ee00034cf944208cb
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-10-26 01:07:33 +0000

    恢复jung

---
+++ java_study/jung/cluster/src/edu/zju/cs/ooobgy/graph/ClusterGraph.java
@@ -0,0 +1,238 @@
+package edu.zju.cs.ooobgy.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.collections15.Transformer;
+
+import edu.zju.cs.ooobgy.algo.cluster.WeakComponentClusterer;
+import edu.zju.cs.ooobgy.algo.util.Pair;
+import edu.zju.cs.ooobgy.graph.util.EdgeType;
+import edu.zju.cs.ooobgy.graph.weight.EdgesWeight;
+
+/**
+ * 在聚类算法中常用的一个Graph描述的实现，
+ * 这个实现包含图的基本操作，默认地，这个图是：无向的、有权的.</br>
+ * <li>无向是已经实现的特性，无法继续扩充为有向</li>
+ * <li>权的实现依赖于V,E的具体实现</li>
+ * 该类也实现了存储Weak Components,允许通过{@link WeakComponentGraph#getComponents()}快速得到弱连通分量
+ * @author frogcherry 周晓龙
+ * @created 2010-11-30
+ * @Email frogcherry@gmail.com
+ * @see WeakComponentClusterer
+ * @see Graph
+ * @see WeakComponentGraph
+ */
+public class ClusterGraph<V, E> extends WeakComponentGraph<V, E> implements WeightedGraph<V, E>, UndirectedGraph<V, E>{
+	private Map<V, Set<E>> vertices;//vertex为key,edges为value
+	private Map<E, Pair<V>> edges;//edge为key,vertex为value
+	private Transformer<E, ? extends Number> edge_weights;//边的权重
+
+	/**
+	 * 使用已知的图的表示构件进行构造，不保证依赖的完备性，请谨慎使用，使用时许哟确保传入的构件配套
+	 * @param vertices
+	 * @param edges
+	 * @param edge_weights
+	 */
+	public ClusterGraph(Map<V, Set<E>> vertices, Map<E, Pair<V>> edges, 
+			Transformer<E, ? extends Number> edge_weights) {
+		super();
+		this.vertices = vertices;
+		this.edges = edges;
+		this.edge_weights = edge_weights;
+	}
+
+	/**
+	 * 全新空构造
+	 */
+	public ClusterGraph() {
+		super();
+		this.edges = new HashMap<E, Pair<V>>();
+		this.vertices = new HashMap<V, Set<E>>();
+		this.edge_weights = new EdgesWeight<E, Double>(this.getEdges());
+	}
+	
+	/**
+	 * 返回一个图的克隆体,深度克隆，确保数据完整性;克隆深度到达图的边和节点为基本元素
+	 */
+	@Override
+	public ClusterGraph<V, E> clone() {
+		ClusterGraph<V, E> clonedGraph = new ClusterGraph<V, E>();
+		EdgesWeight<E, Double> edgesWeights = new EdgesWeight<E, Double>();
+		for(E e : this.edges.keySet()){
+			Pair<V> endPoints = this.getEndpoints(e);
+			clonedGraph.addEdge(e, endPoints.getFirst(), endPoints.getSecond());
+			edgesWeights.addEdgeWeight(e, this.getEdgeWeight(e).doubleValue());
+		}
+		
+		clonedGraph.setEdge_weights(edgesWeights);
+		
+		return clonedGraph;
+	}
+
+	@Override
+	public Set<? extends V> getVertices() {
+		return this.vertices.keySet();
+	}
+
+	@Override
+	public Set<E> getEdges() {
+		return this.edges.keySet();
+	}
+
+	public Set<E> getEdges(V vertex) {
+		return this.vertices.get(vertex);
+	}
+	
+	@Override
+	public Set<E> getInEdges(V vertex) {
+		return getEdges(vertex);
+	}
+
+	@Override
+	public Set<E> getOutEdges(V vertex) {
+		return getEdges(vertex);
+	}
+
+	@Override
+	public Set<V> getPredecessors(V vertex) {
+		return getNeighbors(vertex);
+	}
+
+	@Override
+	public Set<V> getSuccessors(V vertex) {
+		return getNeighbors(vertex);
+	}
+
+	/**
+	 * 由于图无向，所以对于edge的source，返回第1个，谨慎使用
+	 */
+	@Override
+	@Deprecated
+	public V getSource(E directed_edge) {
+		return getEndpoints(directed_edge).getFirst();
+	}
+
+	/**
+	 * 由于图无向，所以对于edge的dest，返回第2个，谨慎使用
+	 */
+	@Override
+	@Deprecated
+	public V getDest(E directed_edge) {
+		return getEndpoints(directed_edge).getSecond();
+	}
+
+	@Override
+	public Pair<V> getEndpoints(E edge) {
+		return this.edges.get(edge);
+	}
+
+	@Override
+	public V getOpposite(V vertex, E edge) {
+		return getEndpoints(edge).getAnother(vertex);
+	}
+
+	@Override
+	public Set<V> getNeighbors(V vertex) {
+		Set<V> result = new HashSet<V>();
+		Set<E> edges = getEdges(vertex);
+		for (E e : edges) {
+			result.add(getOpposite(vertex, e));
+		}
+		return result;
+	}
+
+	@Override
+	protected boolean addVertex2WC(V vertex) {
+		if (containsVertex(vertex)) {
+			return false;
+		}else{
+			this.vertices.put(vertex, new HashSet<E>());
+			return true;
+		}
+	}
+
+	@Override
+	protected boolean addEdge2WC(E e, V v1, V v2) {
+		if (containsEdge(e)) {
+			return false;
+		} else {
+			Pair<V> endPoints = new Pair<V>(v1, v2);
+			addVertex2WC(v1);
+			addVertex2WC(v2);
+			this.edges.put(e, endPoints);
+			this.vertices.get(v1).add(e);
+			this.vertices.get(v2).add(e);
+			return true;
+		}
+	}
+
+	@Override
+	protected boolean removeVertex2WC(V vertex) {
+		if (containsVertex(vertex)) {
+			Set<E> v_edges = getEdges(vertex);
+			for (E e : v_edges) {
+				removeEdge2WC(e);
+			}
+			this.vertices.remove(vertex);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	protected boolean removeEdge2WC(E edge) {
+		if (containsEdge(edge)) {
+			Pair<V> endPoints = getEndpoints(edge);
+			for (V v : endPoints) {
+				this.vertices.get(v).remove(edge);
+			}
+			this.edges.remove(edge);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Map<E, Pair<V>> getEdgeMap(){
+		return this.edges;
+	}
+	
+	@Override
+	public Transformer<E, ? extends Number> getEdgeWeights() {
+		return  edge_weights;
+	}
+
+	public void setEdge_weights(Transformer<E, ? extends Number> edge_weights) {
+		this.edge_weights = edge_weights;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder graphStr = new StringBuilder();
+		graphStr.append("{\nClusterGraph: \n");
+		
+		for (E e : this.edges.keySet()) {
+			graphStr.append("[ ");
+			graphStr.append(e).append(", ");
+			graphStr.append(this.getEndpoints(e)).append(", ");
+			graphStr.append(this.getEdgeWeight(e));
+			graphStr.append(" ]\n");
+		}
+		
+		graphStr.append("}");
+		return graphStr.toString();
+	}
+
+	public Number getEdgeWeight(E e) {
+		return edge_weights.transform(e);
+	}
+
+	@Override
+	public EdgeType getEdgeType(E e) {
+		return EdgeType.UNDIRECTED;
+	}
+}
---
+++ java_study/jung/cluster/test/edu/zju/cs/ooobgy/algo/cluster/qualify/NMIQualifyTest.java
@@ -0,0 +1,118 @@
+package edu.zju.cs.ooobgy.algo.cluster.qualify;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class NMIQualifyTest extends TestCase{
+	
+	@Test
+	public void testNMI(){
+		int i = 0;
+		System.err.println(UUID.randomUUID());
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2},{3,4}};
+			Integer[][] cbm = {{1,2},{3,4}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2,},{3,4,5},{6}};
+			Integer[][] cbm = {{1,2},{3,4,5},{6}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2,},{3,4,5},{6}};
+			Integer[][] cbm = {{1,2},{3,4,5,6}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2},{3,4}};
+			Integer[][] cbm = {{1,3},{2,4}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2,3},{4,5}};
+			Integer[][] cbm = {{1,2},{3,4,5}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2,3,4,5}};
+			Integer[][] cbm = {{1,2,3,4,5}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1,2,3}};
+			Integer[][] cbm = {{1,2,3}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1}};
+			Integer[][] cbm = {{1}};
+			nmiCase(cam, cbm);
+		}
+		
+		System.out.println("========= case " + i + " =========");
+		i++;
+		{
+			Integer[][] cam = {{1}};
+			Integer[][] cbm = {{1,2}};
+			nmiCase(cam, cbm);
+		}
+	}
+
+	private void nmiCase(Integer[][] cam, Integer[][] cbm) {
+		Set<Set<Integer>> ca = makeSet(cam); 
+		Set<Set<Integer>> cb = makeSet(cbm);
+		
+		System.out.println("ca = " + ca);
+		System.out.println("cb = " + cb);
+		
+		@SuppressWarnings("rawtypes")
+		NMIQualify qualify = new NMIQualify<Integer>();
+		@SuppressWarnings("unchecked")
+		double nmi = qualify.qualifyNMI(ca, cb);
+		
+		System.out.println("NMI(ca,cb) = " + nmi);
+	}
+
+	private Set<Set<Integer>> makeSet(Integer[][] cm) {
+		Set<Set<Integer>> cs = new HashSet<Set<Integer>>();
+		for (Integer[] c : cm) {
+			Set<Integer> cc = new HashSet<Integer>();
+			for (Integer v : c) {
+				cc.add(v);
+			}
+			cs.add(cc);
+		}
+		
+		return cs;
+	}
+}
