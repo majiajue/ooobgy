b0a1631151362dd0b3a374097e83efe04a157cb3
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2011-10-26 01:23:41 +0000

    恢复jung

---
+++ trunk/java_study/jung/visualization/src/edu/zju/cs/ooobgy/visualization/platform/ClusterPlatform.java
@@ -0,0 +1,318 @@
+package edu.zju.cs.ooobgy.visualization.platform;
+
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JToggleButton;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.apache.commons.collections15.Factory;
+import org.apache.commons.collections15.Transformer;
+import org.apache.commons.collections15.functors.ConstantTransformer;
+import org.apache.commons.collections15.functors.MapTransformer;
+import org.apache.commons.collections15.map.LazyMap;
+
+import edu.uci.ics.jung.algorithms.layout.util.Relaxer;
+import edu.zju.cs.ooobgy.algo.cluster.EdgeBetweennessClusterer;
+import edu.zju.cs.ooobgy.dt.db.ClusterGraphDBLoader;
+import edu.zju.cs.ooobgy.graph.ClusterGraph;
+import edu.zju.cs.ooobgy.graph.Graph;
+import edu.zju.cs.ooobgy.visualization.GraphZoomScrollPane;
+import edu.zju.cs.ooobgy.visualization.VisualizationViewer;
+import edu.zju.cs.ooobgy.visualization.control.DefaultModalGraphMouse;
+import edu.zju.cs.ooobgy.visualization.layout.AggregateLayout;
+import edu.zju.cs.ooobgy.visualization.layout.CircleLayout;
+import edu.zju.cs.ooobgy.visualization.layout.FRLayout;
+import edu.zju.cs.ooobgy.visualization.layout.Layout;
+
+
+/**
+ * 
+ * @author frogcherry 周晓龙
+ * @created 2011-2-24
+ * @Email frogcherry@gmail.com
+ */
+@SuppressWarnings({ "serial", "unchecked" })
+public class ClusterPlatform extends JApplet {
+	VisualizationViewer<String, Integer> vv;
+	
+//	Factory<Graph<String, Integer>> graphFactory;
+	
+	Map<String,Paint> vertexPaints = 
+		LazyMap.<String,Paint>decorate(new HashMap<String,Paint>(),
+				new ConstantTransformer(Color.white));
+	Map<Integer,Paint> edgePaints =
+	LazyMap.<Integer,Paint>decorate(new HashMap<Integer,Paint>(),
+			new ConstantTransformer(Color.blue));
+
+	public final Color[] similarColors =
+	{
+		new Color(216, 134, 134),
+		new Color(135, 137, 211),
+		new Color(134, 206, 189),
+		new Color(206, 176, 134),
+		new Color(194, 204, 134),
+		new Color(145, 214, 134),
+		new Color(133, 178, 209),
+		new Color(103, 148, 255),
+		new Color(60, 220, 220),
+		new Color(30, 250, 100)
+	};
+	
+	public static void main(String[] args) throws IOException {
+		
+		ClusterPlatform cd = new ClusterPlatform();
+		cd.start();
+		// Add a restart button so the graph can be redrawn to fit the size of the frame
+		JFrame jf = new JFrame();
+		jf.getContentPane().add(cd);
+		
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.pack();
+		jf.setVisible(true);
+	}
+	
+	public void start() {
+//		InputStream is = this.getClass().getClassLoader().getResourceAsStream("datasets/zachary.net");
+		try
+        {        
+            setUpView();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error in loading graph");
+            e.printStackTrace();
+        }
+	}
+
+	private void setUpView() throws IOException {
+		
+    	Factory<Number> vertexFactory = new Factory<Number>() {
+            int n = 0;
+            public Number create() { return n++; }
+        };
+        Factory<Number> edgeFactory = new Factory<Number>()  {
+            int n = 0;
+            public Number create() { return n++; }
+        };
+
+        ClusterGraphDBLoader dbLoader = new ClusterGraphDBLoader();
+		ClusterGraph<String, Integer> graph = dbLoader.load("001001");
+
+		//Create a simple layout frame
+        //specify the Fruchterman-Rheingold layout algorithm
+        final AggregateLayout<String, Integer> layout = 
+        	new AggregateLayout<String, Integer>(new FRLayout<String, Integer>(graph));
+
+		vv = new VisualizationViewer<String, Integer>(layout);
+		vv.setBackground( Color.white );
+		//Tell the renderer to use our own customized color rendering
+		vv.getRenderContext().setVertexFillPaintTransformer(MapTransformer.<String,Paint>getInstance(vertexPaints));
+		vv.getRenderContext().setVertexDrawPaintTransformer(new Transformer<String,Paint>() {
+			public Paint transform(String v) {
+				if(vv.getPickedVertexState().isPicked(v)) {
+					return Color.cyan;
+				} else {
+					return Color.BLACK;
+				}
+			}
+		});
+
+		vv.getRenderContext().setEdgeDrawPaintTransformer(MapTransformer.<Integer,Paint>getInstance(edgePaints));
+
+		vv.getRenderContext().setEdgeStrokeTransformer(new Transformer<Integer,Stroke>() {
+                protected final Stroke THIN = new BasicStroke(1);
+                protected final Stroke THICK= new BasicStroke(2);
+                public Stroke transform(Integer e)
+                {
+                    Paint c = edgePaints.get(e);
+                    if (c == Color.LIGHT_GRAY)
+                        return THIN;
+                    else 
+                        return THICK;
+                }
+            });
+
+		//add restart button
+		JButton scramble = new JButton("Restart");
+		scramble.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Layout layout = vv.getGraphLayout();
+				layout.initialize();
+				Relaxer relaxer = vv.getModel().getRelaxer();
+				if(relaxer != null) {
+					relaxer.stop();
+					relaxer.prerelax();
+					relaxer.relax();
+				}
+			}
+
+		});
+		
+		DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+		vv.setGraphMouse(gm);
+		
+		final JToggleButton groupVertices = new JToggleButton("Group Clusters");
+
+		//Create slider to adjust the number of edges to remove when clustering
+		final JSlider edgeBetweennessSlider = new JSlider(JSlider.HORIZONTAL);
+        edgeBetweennessSlider.setBackground(Color.WHITE);
+		edgeBetweennessSlider.setPreferredSize(new Dimension(210, 50));
+		edgeBetweennessSlider.setPaintTicks(true);
+		edgeBetweennessSlider.setMaximum(graph.getEdgeCount());
+		edgeBetweennessSlider.setMinimum(0);
+		edgeBetweennessSlider.setValue(0);
+		edgeBetweennessSlider.setMajorTickSpacing(10);
+		edgeBetweennessSlider.setPaintLabels(true);
+		edgeBetweennessSlider.setPaintTicks(true);
+
+//		edgeBetweennessSlider.setBorder(BorderFactory.createLineBorder(Color.black));
+		//TO DO: edgeBetweennessSlider.add(new JLabel("Node Size (PageRank With Priors):"));
+		//I also want the slider value to appear
+		final JPanel eastControls = new JPanel();
+		eastControls.setOpaque(true);
+		eastControls.setLayout(new BoxLayout(eastControls, BoxLayout.Y_AXIS));
+		eastControls.add(Box.createVerticalGlue());
+		eastControls.add(edgeBetweennessSlider);
+
+		final String COMMANDSTRING = "Edges removed for clusters: ";
+		final String eastSize = COMMANDSTRING + edgeBetweennessSlider.getValue();
+		
+		final TitledBorder sliderBorder = BorderFactory.createTitledBorder(eastSize);
+		eastControls.setBorder(sliderBorder);
+		//eastControls.add(eastSize);
+		eastControls.add(Box.createVerticalGlue());
+		
+		groupVertices.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+					clusterAndRecolor(layout, edgeBetweennessSlider.getValue(), 
+							similarColors, e.getStateChange() == ItemEvent.SELECTED);
+					vv.repaint();
+			}});
+
+
+		clusterAndRecolor(layout, 0, similarColors, groupVertices.isSelected());
+
+		edgeBetweennessSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				JSlider source = (JSlider) e.getSource();
+				if (!source.getValueIsAdjusting()) {
+					int numEdgesToRemove = source.getValue();
+					clusterAndRecolor(layout, numEdgesToRemove, similarColors,
+							groupVertices.isSelected());
+					sliderBorder.setTitle(
+						COMMANDSTRING + edgeBetweennessSlider.getValue());
+					eastControls.repaint();
+					vv.validate();
+					vv.repaint();
+				}
+			}
+		});
+
+		Container content = getContentPane();
+		content.add(new GraphZoomScrollPane(vv));
+		JPanel south = new JPanel();
+		JPanel grid = new JPanel(new GridLayout(2,1));
+		grid.add(scramble);
+		grid.add(groupVertices);
+		south.add(grid);
+		south.add(eastControls);
+		JPanel p = new JPanel();
+		p.setBorder(BorderFactory.createTitledBorder("Mouse Mode"));
+		p.add(gm.getModeComboBox());
+		south.add(p);
+		content.add(south, BorderLayout.SOUTH);
+	}
+
+	public void clusterAndRecolor(AggregateLayout<String, Integer> layout,
+		int numEdgesToRemove,
+		Color[] colors, boolean groupClusters) {
+		//Now cluster the vertices by removing the top 50 edges with highest betweenness
+		//		if (numEdgesToRemove == 0) {
+		//			colorCluster( g.getVertices(), colors[0] );
+		//		} else {
+		
+		Graph<String, Integer> g = layout.getGraph();
+        layout.removeAll();
+
+		EdgeBetweennessClusterer<String, Integer> clusterer =
+			new EdgeBetweennessClusterer<String, Integer>(numEdgesToRemove);
+		Set<Set<String>> clusterSet = clusterer.transform(g);
+		List<Integer> edges = clusterer.getEdgesRemoved();
+		//恢复图
+		clusterer.recoverGraph();
+		
+		int i = 0;
+		//Set the colors of each node so that each cluster's vertices have the same color
+		for (Iterator<Set<String>> cIt = clusterSet.iterator(); cIt.hasNext();) {
+
+			Set<String> vertices = cIt.next();
+			Color c = colors[i % colors.length];
+
+			colorCluster(vertices, c);
+			if(groupClusters == true) {
+				groupCluster(layout, vertices);
+			}
+			i++;
+		}
+		for (Integer e : g.getEdges()) {
+
+			if (edges.contains(e)) {
+				edgePaints.put(e, Color.lightGray);
+			} else {
+				edgePaints.put(e, Color.black);
+			}
+		}
+
+	}
+
+	private void colorCluster(Set<String> vertices, Color c) {
+		for (String v : vertices) {
+			vertexPaints.put(v, c);
+		}
+	}
+	
+	private void groupCluster(AggregateLayout<String, Integer> layout, Set<String> vertices) {
+		if(vertices.size() < layout.getGraph().getVertexCount()) {
+			Point2D center = layout.transform(vertices.iterator().next());
+			Graph<String, Integer> subGraph = new ClusterGraph<String, Integer>();
+			for(String v : vertices) {
+				subGraph.addVertex(v);
+			}
+			Layout<String, Integer> subLayout = 
+				new CircleLayout<String, Integer>(subGraph);
+			subLayout.setInitializer(vv.getGraphLayout());
+			subLayout.setSize(new Dimension(40,40));
+
+			layout.put(subLayout,center);
+			vv.repaint();
+		}
+	}
+}
