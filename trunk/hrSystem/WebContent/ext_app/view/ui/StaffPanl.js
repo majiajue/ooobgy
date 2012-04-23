fec3ff67d2a9e00898c251194ece22f22ed1b07c
frogcherry <frogcherry@88e2b27d-b25a-a4fe-6dd4-15a81e2c032a>
2012-04-23 02:56:49 +0000

    clear

--- trunk/hrSystem/WebContent/ext_app/view/ui/StaffPanl.js
+++ trunk/hrSystem/WebContent/ext_app/view/ui/StaffPanl.js
@@ -23,89 +23,89 @@ Ext.define('FcSVNAdmin.view.ui.StaffPanl', {
 				xtype : 'gridcolumn',
 				dataIndex : 'id',
 				text : 'id',
-				width : 250
+				width : 50
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'name',
 				text : '姓名',
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'login',
 				text : 'alias',
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'domainl1_name',
 				text : '分公司',
 				renderer : fcDataFilterNull,
-				width : 250
+				width : 150
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'domainl2_name',
 				renderer : fcDataFilterNull,
 				text : '部门',
-				width : 150
+				width : 120
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'domainl3_name',
 				renderer : fcDataFilterNull,
 				text : '业务线',
-				width : 150
+				width : 120
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'email',
 				renderer : fcDataFilterNull,
 				text : 'email',
-				width : 250
+				width : 150
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'role_name',
 				text : '角色',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'level_name',
 				text : '职称',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'salary',
 				text : '月薪',
 				renderer : fcDataFilterNull,
-				width : 250
+				width : 80
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'location',
 				text : '位置',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'phone',
 				text : '电话',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 100
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'gender',
 				text : '性别',
 				renderer : getGender,
-				width : 150
+				width : 30
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'age',
 				text : '年龄',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 50
 			}, {
 				xtype : 'gridcolumn',
 				dataIndex : 'checkin',
 				text : '入职时间',
 				renderer : fcDataFilterNull,
-				width : 150
+				width : 220
 			} ],
 			viewConfig : {
 				forceFit : true
